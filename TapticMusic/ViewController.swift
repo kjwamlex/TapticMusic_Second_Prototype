9b43b90755006df1781c5639592f9c0ee41b8832
Joonwoo Kim <Joonwoo@MacBook-Pro.local>
2017-02-14 09:16:45 -0500

    Initial Commit

---
+++ TapticMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapticMusic
+//
+//  Created by Joonwoo Kim on 2017. 2. 14..
+//  Copyright © 2017년 Joonwoo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
